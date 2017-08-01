dbbb16040eff5aa53e5fd3f2c996f9387d21d40f
Benedicte Raae <benedicte@lillylabs.no>
2017-08-01 11:52:41 +0200

    Download button added (#8)
    Solved #6

--- main.js
+++ main.js
@@ -1,3 +1,63 @@
+// Input and buttons
+////////////////////////
+////////////////////////
+var $controls = document.querySelector('#controls');
+var $download = document.querySelector('#download');
+
+function setControlsState(isActive) {
+  if (isActive) {
+    $controls.classList.add('active');
+  } else {
+    $controls.classList.remove('active');
+  }
+}
+
+function setDownloadHref() {
+  var url = "data:image/svg+xml;utf8," + encodeURIComponent(paper.project.exportSVG({ asString: true }));
+  $download.href = url;
+}
+
+function drawingCompleted() {
+  setControlsState(true);
+  setDownloadHref();
+}
+
+// Art generator
+////////////////////////
+////////////////////////
+
+// Draw line function
+var drawLine = function (point, length, color, angle) {
+  var path = new Path();
+  var start = new Point(point.x, point.y);
+  path.strokeColor = color;
+  path.moveTo(start);
+
+  switch (angle) {
+    case 0:
+      // up
+      point.x = Math.max(point.x - length, MIN_X);
+      break;
+    case 1:
+      // down
+      point.x = Math.min(point.x + length, MAX_X);
+      break;
+    case 2:
+      // left
+      point.y = Math.max(point.y - length, MIN_Y);
+      break;
+    case 3:
+      // right
+      point.y = Math.min(point.y + length, MAX_Y);
+      break;
+    default:
+      console.log("In deafult, something is wrong");
+      break;
+  };
+  path.lineTo(point.x, point.y);
+  return point;
+}
+
 // Setting up stuff that is needed
 var COLORS = ['red', 'blue', 'green', 'purple', 'pink', 'yellow'];
 var STEPS = 200;
@@ -32,38 +92,6 @@ console.log('lengths', lengths);
 console.log('angles', angles);
 console.log('colors', colors);
 
-// Draw line function
-function drawLine(point, length, color, angle) {
-  var path = new Path();
-  var start = new Point(point.x, point.y);
-  path.strokeColor = color;
-  path.moveTo(start);
-
-  switch (angle) {
-    case 0:
-      // up
-      point.x = Math.max(point.x - length, MIN_X);
-      break;
-    case 1:
-      // down
-      point.x = Math.min(point.x + length, MAX_X);
-      break;
-    case 2:
-      // left
-      point.y = Math.max(point.y - length, MIN_Y);
-      break;
-    case 3:
-      // right
-      point.y = Math.min(point.y + length, MAX_Y);
-      break;
-    default:
-      console.log("In deafult, something is wrong");
-      break;
-  };
-  path.lineTo(point.x, point.y);
-  return point;
-}
-
 // Drawing
 var point = {
   x: START_X,
@@ -77,6 +105,8 @@ function loopIt() {
     point = drawLine(point, lengths[i], colors[i], angles[i]);
     setTimeout(loopIt, WAIT_TIME);
     i++;
+  } else {
+    drawingCompleted();
   }
 };
 loopIt();
