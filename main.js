2ee91ee23e7e4aa09e55fc41ba2b7c7fe88c5d94
Benedicte Raae <benedicte@lillylabs.no>
2017-08-02 11:58:08 +0200

    Input (#13)
    * Number of lines can be inputted, solves #5
    * Wait time can be inputted, solves #12 
    * Format can be inputted, solves #4 
    * Max length of line can be inputted, solves #10

--- main.js
+++ main.js
@@ -1,20 +1,95 @@
+// Constants
+var NUMBER_OF_LINES = 200;
+var WAIT_TIME = 50;
+
+var COLORS = ['#ff002a', '#c04b5e', '#ff002a', '#512930', '#42040f', '#f1abf7', '#e808fc', '#ac21b9', '#4a0f4f', '#5e4460', '#b79cf1', '#5203fe', '#280476', '#4b425f', '#170244', '#c4fefd', '#00fffb', '#78b9b8', '#007270', '#2f4242', '#bfffd9', '#00ff66', '#6fb58b', '#00712d', '#2d4737', '#cfffb5', '#59ff00', '#82bd62', '#2f8700', '#354e28', '#f8ff97', '#eeff00', '#b9c058', '#7a8300', '#424425', '#ffed9c', '#ffd000', '#bba856', '#6b5700', '#473f1f', '#ffdec1', '#fe7700', '#a3744b', '#7b3900', '#432d19', '#fadad8', '#ff1100', '#ba483f', '#810900', '#3c1614'];
+
+var FORMAT_WIDTH = 4;
+var FORMAT_HEIGHT = 2;
+
+var MAX_LENGTH = 30; //Other possibilities 100, 200, 400, Math.max(MAX_Y, MAX_X);
+
+
+// Canvas size
+////////////////////////
+////////////////////////
+
+function setCanvasSize(format) {
+  paper.view.viewSize = new Size(paper.view.size.width, paper.view.size.width / format.width * format.height);
+}
+
+setCanvasSize({ width: FORMAT_WIDTH, height: FORMAT_HEIGHT })
+
 // Input and buttons
 ////////////////////////
 ////////////////////////
-var $controls = document.querySelector('#controls');
-var $download = document.querySelector('#download');
+
+var controlsEl = document.querySelector('#controls');
+var downloadButton = document.querySelector('#download');
+
+var numberOfLinesInput = document.querySelector('#number-of-lines');
+numberOfLinesInput.value = NUMBER_OF_LINES;
+
+var waitTimeInput = document.querySelector('#wait-time');
+waitTimeInput.value = WAIT_TIME;
+
+var widthInput = document.querySelector('#width');
+widthInput.value = FORMAT_WIDTH;
+
+var heightInput = document.querySelector('#height');
+heightInput.value = FORMAT_HEIGHT;
+
+widthInput.addEventListener('change', function () {
+  setCanvasSize({
+    width: widthInput.value,
+    height: heightInput.value
+  });
+})
+
+heightInput.addEventListener('change', function () {
+  setCanvasSize({
+    width: widthInput.value,
+    height: heightInput.value
+  });
+})
+
+var lineLengthProcentageInput = document.querySelector('#line-length-procentage');
+lineLengthProcentageInput.value = MAX_LENGTH;
+
+var drawButton = document.querySelector('#draw');
+drawButton.addEventListener('click', function () {
+  var lineLengthSideInput = document.querySelector('input[name="line-length-side"]:checked');
+  var options = {
+    numberOfLines: numberOfLinesInput.value,
+    waitTime: waitTimeInput.value,
+    maxLength: (lineLengthProcentageInput.value / 100) * (lineLengthSideInput.value === 'height' ? paper.view.size.height - 20 : paper.view.size.width - 20),
+    colors: COLORS,
+    size: {
+      min: {
+        x: 10,
+        y: 10,
+      },
+      max: {
+        x: paper.view.size.width - 10,
+        y: paper.view.size.height - 10,
+      }
+    }
+  };
+  paper.project.clear();
+  drawOnCanvas(options);
+});
 
 function setControlsState(isActive) {
   if (isActive) {
-    $controls.classList.add('active');
+    controlsEl.classList.add('active');
   } else {
-    $controls.classList.remove('active');
+    controlsEl.classList.remove('active');
   }
 }
 
 function setDownloadHref() {
   var url = "data:image/svg+xml;utf8," + encodeURIComponent(paper.project.exportSVG({ asString: true }));
-  $download.href = url;
+  downloadButton.href = url;
 }
 
 function drawingCompleted() {
@@ -27,7 +102,7 @@ function drawingCompleted() {
 ////////////////////////
 
 // Draw line function
-var drawLine = function (point, length, color, angle) {
+var drawLine = function (point, length, color, angle, size) {
   var path = new Path();
   var start = new Point(point.x, point.y);
   path.strokeColor = color;
@@ -36,19 +111,19 @@ var drawLine = function (point, length, color, angle) {
   switch (angle) {
     case 0:
       // up
-      point.x = Math.max(point.x - length, MIN_X);
+      point.x = Math.max(point.x - length, size.min.x);
       break;
     case 1:
       // down
-      point.x = Math.min(point.x + length, MAX_X);
+      point.x = Math.min(point.x + length, size.max.x);
       break;
     case 2:
       // left
-      point.y = Math.max(point.y - length, MIN_Y);
+      point.y = Math.max(point.y - length, size.min.y);
       break;
     case 3:
       // right
-      point.y = Math.min(point.y + length, MAX_Y);
+      point.y = Math.min(point.y + length, size.max.y);
       break;
     default:
       console.log("In deafult, something is wrong");
@@ -58,56 +133,48 @@ var drawLine = function (point, length, color, angle) {
   return point;
 }
 
-// Setting up stuff that is needed
-var COLORS = ['#ff002a', '#c04b5e', '#ff002a', '#512930', '#42040f', '#f1abf7', '#e808fc', '#ac21b9', '#4a0f4f', '#5e4460', '#b79cf1', '#5203fe', '#280476', '#4b425f', '#170244', '#c4fefd', '#00fffb', '#78b9b8', '#007270', '#2f4242', '#bfffd9', '#00ff66', '#6fb58b', '#00712d', '#2d4737', '#cfffb5', '#59ff00', '#82bd62', '#2f8700', '#354e28', '#f8ff97', '#eeff00', '#b9c058', '#7a8300', '#424425', '#ffed9c', '#ffd000', '#bba856', '#6b5700', '#473f1f', '#ffdec1', '#fe7700', '#a3744b', '#7b3900', '#432d19', '#fadad8', '#ff1100', '#ba483f', '#810900', '#3c1614'];
+function generateRandomValues(options) {
+  var lengths = [];
+  var angles = [];
+  var colors = [];
 
-var STEPS = 200;
+  for (var i = 0; i < options.numberOfLines; i++) {
+    lengths.push(Math.random() * options.maxLength);
+    angles.push(Math.floor((Math.random() * 4)));
+    colors.push(options.colors[Math.floor((Math.random() * options.colors.length))]);
+  }
 
-var WAIT_TIME = 100;
-
-var MIN_X = 10;
-var MAX_X = view.size.width - 10;
-
-var MIN_Y = 10;
-var MAX_Y = view.size.height - 10;
-
-var MAX_LENGTH = 50; //Other possibilities 100, 200, 400, Math.max(MAX_Y, MAX_X);
-
-var START_X = Math.floor(Math.random() * MAX_X);
-var START_Y = Math.floor(Math.random() * MAX_Y);
-
-var lengths = [];
-var angles = [];
-var colors = [];
-
-for (var i = 0; i < STEPS; i++) {
-  lengths.push(Math.random() * MAX_LENGTH);
-  angles.push(Math.floor((Math.random() * 4)));
-  colors.push(COLORS[Math.floor((Math.random() * COLORS.length))]);
+  return {
+    lengths: lengths,
+    angles: angles,
+    colors: colors
+  }
 }
 
-// Logging key variables
-console.log('START', START_X, START_Y);
-console.log('size', MAX_X, MAX_Y);
-console.log('lengths', lengths);
-console.log('angles', angles);
-console.log('colors', colors);
-
-// Drawing
-var point = {
-  x: START_X,
-  y: START_Y
-};
-
-var i = 0;
-function loopIt() {
-  if (i < STEPS) {
-    // console.log('Tegne strek', i, lengths[i], colors[i], angles[i]);
-    point = drawLine(point, lengths[i], colors[i], angles[i]);
-    setTimeout(loopIt, WAIT_TIME);
-    i++;
+function loopIt(i, point, options, randoms) {
+  if (i < options.numberOfLines) {
+    point = drawLine(point, randoms.lengths[i], randoms.colors[i], randoms.angles[i], options.size);
+    if (options.waitTime > 0) {
+      setTimeout(function () {
+        loopIt(i++, point, options, randoms);
+      }, options.waitTime);
+    } else {
+      loopIt(i++, point, options, randoms);
+    }
   } else {
     drawingCompleted();
   }
 };
-loopIt();
+
+function drawOnCanvas(options) {
+  setControlsState(false);
+  var randoms = generateRandomValues(options);
+  var point = {
+    x: Math.floor(Math.random() * paper.view.size.width - 10),
+    y: Math.floor(Math.random() * paper.view.size.height)
+  };
+
+  console.log('Options', options);
+  console.log('Randoms', randoms);
+  loopIt(0, point, options, randoms);
+}
